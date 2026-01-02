f655f6c53fde8673dafafd8dede33f10f2a6a35f
Krishna Deo Choudhary <raja8340kumar@gmail.com>
2026-01-02 22:03:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const scrollY = window.pageYOffset;
+
+      if (
+        scrollY >= sectionTop - sectionHeight / 3 &&
+        scrollY < sectionTop + sectionHeight - sectionHeight / 3
+      ) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove("active");
+      if (link.getAttribute("href") === "#" + current) {
+        link.classList.add("active");
+      }
+    });
+  });
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("shrink");
+    } else {
+      navbar.classList.remove("shrink");
+    }
+  });
